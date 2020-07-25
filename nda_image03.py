545987b05c997ceedfc59bb442cd174c177fb9ed
Nathan Muncy <nmuncy@fiu.edu>
2020-07-25 16:57:40 -0400

    stuck on bash subprocess

--- nda_image03.py
+++ nda_image03.py
@@ -3,6 +3,7 @@
 
 ### get modules
 import os
+import shutil
 import csv
 import subprocess
 import tarfile
@@ -17,9 +18,6 @@ outDir = "/home/nate/Projects/emu-project/mri_pipeline/ndaOutdir"
 ndaTemplate = os.path.join(outDir,"image03_template.csv")
 outFile = os.path.join(outDir,"test.csv")
 
-
-# ndaTemplate = outDir + "/image03_template.csv"
-# outFile = outDir + "/test.csv"
 
 
 
@@ -48,11 +46,11 @@ def MkJsonList(subj, sess, scan):
 # print(MkJsonList("sub-4002", "ses-S1", "anat"))
 
 # from csv import DictWriter
-def append_dict_as_row(file_name, dict_of_elem, field_names):
-    with open(file_name, 'a+') as write_obj:
-    	dict_writer = csv.writer(write_obj)
-        dict_writer = csv.DictWriter(write_obj, fieldnames=field_names)
-        dict_writer.writerow(dict_of_elem)
+# def append_dict_as_row(file_name, dict_of_elem, field_names):
+#     with open(file_name, 'a+') as write_obj:
+#     	dict_writer = csv.writer(write_obj)
+#         dict_writer = csv.DictWriter(write_obj, fieldnames = field_names)
+#         dict_writer.writerow(dict_of_elem)
 
 
 
@@ -66,9 +64,9 @@ ndaColumns = holdCols[0]
 
 
 # start new csv
-with open(outFile, 'wb') as file:
-	writer = csv.writer(file)
-	writer.writerow(ndaColumns)
+# with open(outFile, 'wb') as file:
+# 	writer = csv.writer(file)
+# 	writer.writerow(ndaColumns)
 
 
 ### Mine files
@@ -82,30 +80,41 @@ for i in subjList:
 
 		sesNum = j[4:]
 		string = "McMakin_EMU-000-R01_" + subNum + sesNum + "-" + sesNum + ".tar.gz"
-		tarBall = tarfile.open(os.path.join(dicomDir,string))
-		tarBall.extractall(outDir)
-		# tarBall.close()
 
-		# make temp dict
-		tarPath = os.path.join(outDir, "scratch/akimb009/McMakin_EMU", string[:-7], "scans")
-		# print(tarPath)
+		# temporarily unpack small reference dir from tar ball
+		tarBall = tarfile.open(os.path.join(dicomDir,string),'r')
+		for member in tarBall.getmembers():
+			if "1-localizer_32ch" in member.name:
+				tarBall.extract(member, outDir)
 
+		dicomPath = os.path.join(os.path.join(outDir, "scratch/akimb009/McMakin_EMU", string[:-7], "scans/1-localizer_32ch/resources/DICOM/files"))
+		dicomList = os.listdir(dicomPath)
+		dicomRef = os.path.join(dicomPath, dicomList[0])
+		print(dicomRef)
 
+		# store dicom header output
+		dicomHead = subprocess.check_call(["dicom_hdr", dicomRef])
+		subprocess.run(["afni -ver"])
+		# output, error = dicomHead.communicate()
+		# print(output)	
 
 		scanList = MkScanList(i, j)
 		for k in scanList:
 
 			jsonList = MkJsonList(i, j, k)
+			# print(jsonList)
 
-			row_dict = {'src_subject_id': i, 'image_file': k}
-			append_dict_as_row(outFile, row_dict, ndaColumns)
+			# row_dict = {'src_subject_id': i, 'image_file': k}
+			# append_dict_as_row(outFile, row_dict, ndaColumns)
 
 
-			# bashCommand = "afni -ver"
-			# process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-			# output, error = process.communicate()
-			# print(output)
+			bashCommand = "afni -ver"
+			process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+			output, error = process.communicate()
+			print(output)
 
+		# clean unpacked tar ball
+		# shutil.rmtree(os.path.join(outDir, "scratch"))
 
 
 
