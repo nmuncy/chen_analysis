1ff92679973527f64aae30a25166e451d1502d27
Nathan Muncy <nmuncy@fiu.edu>
2020-09-09 07:51:41 -0400

    changed resources

--- step1_sbatch_preproc.sh
+++ step1_sbatch_preproc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=01:00:00   # walltime
+#SBATCH --time=05:00:00   # walltime
 #SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=4gb   # memory per CPU core
